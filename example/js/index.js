5484829a09ffb09f95a239fd0c27e7247fb66b25
joakimfr <fritzjoakim@hotmail.com>
2022-11-09 22:33:38 +0100

    j

--- example/js/index.js
+++ example/js/index.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'https://fathomless-shelf-54969.herokuapp.com';
 // API nyckel att använda ifall man enbart siktar på godkänt: solaris-vKkkQHqQboi7c6JF
-
+let planets=  [];
 
 async function getKey() {
     const response = await fetch(`${BASE_URL}/keys`, { method: 'POST' });
@@ -16,7 +16,29 @@ async function getPlanets() {
     });
     const data = await response.json();
     console.log(data);
+
+    planets = data.bodies;
+
+  planets.map((planet) => {
+    document.getElementById(
+      "planeter"
+    ).innerHTML += `<div id="planet${planet.id}">${planet.name}</div>`;
+});
+
+//get more info when clicking on a planet
+planets.map((planet) => {
+    document
+      .getElementById(`planet${planet.id}`)
+      .addEventListener("click", () => {
+        document.getElementById(
+          "planetInfo"
+        ).innerHTML = `${planet.name} is a ${planet.type} and has a latin name of ${planet.latinName}`;
+      });
+  });
 }
+
+
 
 getKey();
 getPlanets();
+
